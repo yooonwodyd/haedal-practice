d0109660a4f29e00b6e33f89e4ae8f1ef4d4346a
yooonwodyd <dbswodyd00@naver.com>
2023-05-04 14:32:34 +0900

    #3 articlerepository 이름변경

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -1,16 +1,21 @@
 package com.example.demo.service;
 
 
+import com.example.demo.entity.Article;
 import com.example.demo.repositroy.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ArticleService {
-    private final ArticleRepository postsRepository;
+    private final ArticleRepository articleRepository;
 
     @Autowired
-    public ArticleService(ArticleRepository postsRepository) {
-        this.postsRepository = postsRepository;
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
     }
+
 }
+
