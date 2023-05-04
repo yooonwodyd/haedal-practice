8210c892fe15651de017215f81254af30703a5db
yooonwodyd <dbswodyd00@naver.com>
2023-05-04 14:30:32 +0900

    #3 생성자 방식으로 의존성 주입.

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
 
+import com.example.demo.repositroy.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
+    private final ArticleRepository postsRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
 }
