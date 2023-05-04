075b564fe9d4a4f8937c9f988d24718811bc19db
yooonwodyd <dbswodyd00@naver.com>
2023-05-04 14:43:05 +0900

    #5 home.html 파일 작성. thymeleaf 사용함

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,28 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entity.Article;
+import com.example.demo.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private final ArticleService articleService;
+
+    @Autowired
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<Article> articles = articleService.articleList();
+        model.addAttribute("articles",articles);
+        return "home";
+    }
 }
